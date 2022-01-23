08a4958df506d900e32b9a407d43103f5917d1d4
Nikolay Kim <fafhrd91@gmail.com>
2022-01-23 21:30:46 +0600

    remove deprecated fn

--- ntex/src/web/types/payload.rs
+++ ntex/src/web/types/payload.rs
@@ -22,7 +22,7 @@ use crate::web::{FromRequest, HttpRequest};
 /// async fn index(mut body: web::types::Payload) -> Result<HttpResponse, error::PayloadError>
 /// {
 ///     let mut bytes = BytesMut::new();
-///     while let Some(item) = ntex::util::next(&mut body).await {
+///     while let Some(item) = ntex::util::stream_recv(&mut body).await {
 ///         bytes.extend_from_slice(&item?);
 ///     }
 ///
@@ -90,7 +90,7 @@ impl Stream for Payload {
 /// async fn index(mut body: web::types::Payload) -> Result<HttpResponse, error::PayloadError>
 /// {
 ///     let mut bytes = BytesMut::new();
-///     while let Some(item) = ntex::util::next(&mut body).await {
+///     while let Some(item) = ntex::util::stream_recv(&mut body).await {
 ///         bytes.extend_from_slice(&item?);
 ///     }
 ///
