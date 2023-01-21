379f96e3a2e0bd72adaedd408b5fe78e50c2bfaa
leon3s <leo.vernisse@gmail.com>
2023-01-21 14:25:31 +0100

    [REFACTOR] Rust fmt errors

--- ntex-service/src/fn_shutdown.rs
+++ ntex-service/src/fn_shutdown.rs
@@ -1,4 +1,4 @@
-use std::{future::Future, marker::PhantomData, cell::Cell, task::{Context, Poll}, pin::Pin};
+use std::{cell::Cell, future::Future, marker::PhantomData, pin::Pin, task::{Context, Poll}};
 
 use crate::Service;
 
@@ -12,14 +12,13 @@ pub fn fn_shutdown<F, Fut, Req, Res, Err>(f: F) -> FnShutdown<F, Fut, Req, Res, 
 where
     F: Fn(Req) -> Fut,
     Fut: Future<Output = Result<Res, Err>>, {
-  FnShutdown::new(f)
+    FnShutdown::new(f)
 }
 
 pub struct FnShutdown<F, Fut, Req, Res, Err, FShut = fn()>
 where
     F: Fn(Req) -> Fut,
-    Fut: Future<Output = Result<Res, Err>>,
-{
+    Fut: Future<Output = Result<Res, Err>>, {
     f: F,
     f_shutdown: Cell<Option<FShut>>,
     _t: PhantomData<Req>,
@@ -28,8 +27,7 @@ where
 impl<F, Fut, Req, Res, Err> FnShutdown<F, Fut, Req, Res, Err>
 where
     F: Fn(Req) -> Fut,
-    Fut: Future<Output = Result<Res, Err>>,
-{
+    Fut: Future<Output = Result<Res, Err>>, {
     pub(crate) fn new(f: F) -> Self {
         Self {
             f,
@@ -96,26 +94,26 @@ where
 
 #[cfg(test)]
 mod tests {
-  use ntex_util::future::lazy;
-  use std::task::Poll;
+    use ntex_util::future::lazy;
+    use std::task::Poll;
 
-  use super::*;
+    use super::*;
 
-  #[ntex::test]
-  async fn test_fn_shutdown() {
-      let mut is_called = false;
-      // The shutdown callback
-      let f_shutdown = || {
-          is_called = true;
-      };
+    #[ntex::test]
+    async fn test_fn_shutdown() {
+        let mut is_called = false;
+        // The shutdown callback
+        let f_shutdown = || {
+            is_called = true;
+        };
 
-      let srv = fn_shutdown(|_| async { Ok::<_, ()>("srv") }).on_shutdown(f_shutdown);
+        let srv = fn_shutdown(|_| async { Ok::<_, ()>("srv") }).on_shutdown(f_shutdown);
 
-      let res = srv.call(()).await;
-      assert_eq!(lazy(|cx| srv.poll_ready(cx)).await, Poll::Ready(Ok(())));
-      assert!(res.is_ok());
-      assert_eq!(res.unwrap(), "srv");
-      assert_eq!(lazy(|cx| srv.poll_shutdown(cx)).await, Poll::Ready(()));
-      assert!(is_called);
-  }
+        let res = srv.call(()).await;
+        assert_eq!(lazy(|cx| srv.poll_ready(cx)).await, Poll::Ready(Ok(())));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), "srv");
+        assert_eq!(lazy(|cx| srv.poll_shutdown(cx)).await, Poll::Ready(()));
+        assert!(is_called);
+    }
 }
