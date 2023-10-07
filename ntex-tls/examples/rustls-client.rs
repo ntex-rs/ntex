f20e8121aba81565c7b1d3a2ba68590f4fae134d
msga-mmm <michael.sga.mmm@gmail.com>
2023-10-07 12:33:00 -0500

    feat: use .add__trust_anchors() instead of .add_server_trust_anchors()

--- ntex-tls/examples/rustls-client.rs
+++ ntex-tls/examples/rustls-client.rs
@@ -10,15 +10,13 @@ async fn main() -> io::Result<()> {
 
     // rustls config
     let mut cert_store = RootCertStore::empty();
-    cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
-        |ta| {
-            OwnedTrustAnchor::from_subject_spki_name_constraints(
-                ta.subject,
-                ta.spki,
-                ta.name_constraints,
-            )
-        },
-    ));
+    cert_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
+        OwnedTrustAnchor::from_subject_spki_name_constraints(
+            ta.subject,
+            ta.spki,
+            ta.name_constraints,
+        )
+    }));
     let config = ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(cert_store)
