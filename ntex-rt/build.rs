53606fd6f51b4177eb14cc1cf2b10572057616ea
Nikolay Kim <fafhrd91@gmail.com>
2025-01-03 21:59:31 +0500

    Relax runtime requirements

--- ntex-rt/build.rs
+++ ntex-rt/build.rs
@@ -1,7 +1,6 @@
 use std::{collections::HashSet, env};
 
 fn main() {
-    let mut clippy = false;
     let mut features = HashSet::<&'static str>::default();
 
     for (key, val) in env::vars() {
@@ -10,24 +9,14 @@ fn main() {
             "CARGO_FEATURE_TOKIO" => features.insert("tokio"),
             "CARGO_FEATURE_GLOMMIO" => features.insert("glommio"),
             "CARGO_FEATURE_ASYNC_STD" => features.insert("async-std"),
-            "CARGO_CFG_FEATURE" => {
-                if val.contains("cargo-clippy") {
-                    clippy = true;
-                }
-                false
-            }
             _ => false,
         };
     }
 
-    if !clippy {
-        if features.is_empty() {
-            panic!("Runtime must be selected '--feature=ntex/$runtime', available options are \"compio\", \"tokio\", \"async-std\", \"glommio\"");
-        } else if features.len() > 1 {
-            panic!(
-                "Only one runtime feature could be selected, current selection {:?}",
-                features
-            );
-        }
+    if features.len() > 1 {
+        panic!(
+            "Only one runtime feature could be selected, current selection {:?}",
+            features
+        );
     }
 }
