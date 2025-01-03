2fc8647dbcf1ad67bdcb7d95e9f4e815cfcee5af
Nikolay Kim <fafhrd91@gmail.com>
2025-01-03 22:01:01 +0500

    wip

--- ntex-rt/build.rs
+++ ntex-rt/build.rs
@@ -3,7 +3,7 @@ use std::{collections::HashSet, env};
 fn main() {
     let mut features = HashSet::<&'static str>::default();
 
-    for (key, val) in env::vars() {
+    for (key, _) in env::vars() {
         let _ = match key.as_ref() {
             "CARGO_FEATURE_COMPIO" => features.insert("compio"),
             "CARGO_FEATURE_TOKIO" => features.insert("tokio"),
