6624b2f2f2300c02689f9aa55c3a24e64bb9a0c7
Nikolay Kim <fafhrd91@gmail.com>
2025-03-16 11:51:51 +0100

    wip

--- ntex/src/web/test.rs
+++ ntex/src/web/test.rs
@@ -701,10 +701,6 @@ where
             Ok(())
         })
     });
-    // wait for server
-    if std::env::var("GITHUB_ACTIONS") == Ok("true".to_string()) {
-        thread::sleep(std::time::Duration::from_millis(350));
-    }
 
     let (system, server, addr) = rx.recv().unwrap();
 
