846e74f84433c4a088002c27267c2afb1ae5cb6f
Nikolay Kim <fafhrd91@gmail.com>
2025-04-14 12:50:17 +0500

    wip

--- ntex-util/src/channel/bstream.rs
+++ ntex-util/src/channel/bstream.rs
@@ -126,7 +126,6 @@ pub struct Sender<E> {
 impl<E> Drop for Sender<E> {
     fn drop(&mut self) {
         if let Some(shared) = self.inner.upgrade() {
-            debug_assert!(!shared.flags.get().intersects(Flags::EOF | Flags::ERROR));
             shared.insert_flag(Flags::EOF);
         }
     }
