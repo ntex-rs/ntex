dd09ac55c69fe96c5d15fccff2410b3885f48753
Nikolay Kim <fafhrd91@gmail.com>
2025-04-18 12:32:11 +0500

    wip

--- ntex/src/http/h1/dispatcher.rs
+++ ntex/src/http/h1/dispatcher.rs
@@ -2,11 +2,9 @@
 use std::task::{ready, Context, Poll};
 use std::{error, future, io, marker, mem, pin::Pin, rc::Rc};
 
-use crate::channel::bstream;
 use crate::io::{Decoded, Filter, Io, IoStatusUpdate, RecvError};
 use crate::service::{PipelineCall, Service};
-use crate::time::Seconds;
-use crate::util::Either;
+use crate::{channel::bstream, time::Seconds, util::Either};
 
 use crate::http::body::{BodySize, MessageBody, ResponseBody};
 use crate::http::error::{PayloadError, ResponseError};
