7d9f6f2ec24aa333e1fc675b83e5c1eddc119f65
Nikolay Kim <fafhrd91@gmail.com>
2025-12-02 09:46:58 +0100

    wip

--- ntex-compio/src/lib.rs
+++ ntex-compio/src/lib.rs
@@ -1,50 +1,59 @@
-use std::{io::Result, net, net::SocketAddr};
-
-use ntex_io::Io;
-use ntex_service::cfg::SharedCfg;
-
+#[cfg(feature = "compio")]
 mod io;
 
-/// Tcp stream wrapper for compio TcpStream
-struct TcpStream(compio_net::TcpStream);
+#[cfg(feature = "compio")]
+mod compat {
+    use std::{io::Result, net, net::SocketAddr};
 
-#[cfg(unix)]
-/// Tcp stream wrapper for compio UnixStream
-struct UnixStream(compio_net::UnixStream);
+    use ntex_io::Io;
+    use ntex_service::cfg::SharedCfg;
 
-/// Opens a TCP connection to a remote host.
-pub async fn tcp_connect(addr: SocketAddr, cfg: SharedCfg) -> Result<Io> {
-    let sock = compio_net::TcpStream::connect(addr).await?;
-    Ok(Io::new(TcpStream(sock), cfg))
+    use super::io;
+
+    /// Tcp stream wrapper for compio TcpStream
+    struct TcpStream(compio_net::TcpStream);
+
+    #[cfg(unix)]
+    /// Tcp stream wrapper for compio UnixStream
+    struct UnixStream(compio_net::UnixStream);
+
+    /// Opens a TCP connection to a remote host.
+    pub async fn tcp_connect(addr: SocketAddr, cfg: SharedCfg) -> Result<Io> {
+        let sock = compio_net::TcpStream::connect(addr).await?;
+        Ok(Io::new(TcpStream(sock), cfg))
+    }
+
+    #[cfg(unix)]
+    /// Opens a unix stream connection.
+    pub async fn unix_connect<'a, P>(addr: P, cfg: SharedCfg) -> Result<Io>
+    where
+        P: AsRef<std::path::Path> + 'a,
+    {
+        let sock = compio_net::UnixStream::connect(addr).await?;
+        Ok(Io::new(UnixStream(sock), cfg))
+    }
+
+    /// Convert std TcpStream to tokio's TcpStream
+    pub fn from_tcp_stream(stream: net::TcpStream, cfg: SharedCfg) -> Result<Io> {
+        stream.set_nodelay(true)?;
+        Ok(Io::new(
+            TcpStream(compio_net::TcpStream::from_std(stream)?),
+            cfg,
+        ))
+    }
+
+    #[cfg(unix)]
+    /// Convert std UnixStream to tokio's UnixStream
+    pub fn from_unix_stream(
+        stream: std::os::unix::net::UnixStream,
+        cfg: SharedCfg,
+    ) -> Result<Io> {
+        Ok(Io::new(
+            UnixStream(compio_net::UnixStream::from_std(stream)?),
+            cfg,
+        ))
+    }
 }
 
-#[cfg(unix)]
-/// Opens a unix stream connection.
-pub async fn unix_connect<'a, P>(addr: P, cfg: SharedCfg) -> Result<Io>
-where
-    P: AsRef<std::path::Path> + 'a,
-{
-    let sock = compio_net::UnixStream::connect(addr).await?;
-    Ok(Io::new(UnixStream(sock), cfg))
-}
-
-/// Convert std TcpStream to tokio's TcpStream
-pub fn from_tcp_stream(stream: net::TcpStream, cfg: SharedCfg) -> Result<Io> {
-    stream.set_nodelay(true)?;
-    Ok(Io::new(
-        TcpStream(compio_net::TcpStream::from_std(stream)?),
-        cfg,
-    ))
-}
-
-#[cfg(unix)]
-/// Convert std UnixStream to tokio's UnixStream
-pub fn from_unix_stream(
-    stream: std::os::unix::net::UnixStream,
-    cfg: SharedCfg,
-) -> Result<Io> {
-    Ok(Io::new(
-        UnixStream(compio_net::UnixStream::from_std(stream)?),
-        cfg,
-    ))
-}
+#[cfg(feature = "compio")]
+pub use self::compat::*;
