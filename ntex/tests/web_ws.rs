5232262bcbb8378a88dda99ded99a4a65012d329
Denis Badurina <badurinadenis@gmail.com>
2026-02-01 08:46:50 +0100

    WebSockets subprotocol support (#735)

--- ntex/tests/web_ws.rs
+++ ntex/tests/web_ws.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use ntex::http::StatusCode;
+use ntex::http::{header, StatusCode};
 use ntex::service::{fn_factory_with_config, fn_service};
 use ntex::util::{ByteString, Bytes};
 use ntex::web::{self, test, ws, App, HttpRequest, HttpResponse};
@@ -161,4 +161,157 @@ async fn web_ws_client() {
 
     // TODO fix
     on_disconnect.await
+}
+
+#[ntex::test]
+async fn web_ws_subprotocol() {
+    use ntex::service::cfg::SharedCfg;
+    use ntex::time::Seconds;
+    use ntex::ws::WsClient;
+
+    let srv = test::server(async || {
+        App::new().service(web::resource("/").route(web::to(
+            |req: HttpRequest| async move {
+                // choose first supported protocol, convert to owned String
+                let protocol: Option<String> = ws::subprotocols(&req)
+                    .find(|p| *p == "my-subprotocol" || *p == "others-subprotocol")
+                    .map(String::from);
+
+                ws::start_using_subprotocol::<_, _, _, web::Error>(
+                    req,
+                    protocol,
+                    fn_factory_with_config(|_| async {
+                        Ok::<_, web::Error>(fn_service(service))
+                    }),
+                )
+                .await
+            },
+        )))
+    })
+    .await;
+
+    // client requests subprotocol
+    let conn = WsClient::build(srv.url("/"))
+        .address(srv.addr())
+        .timeout(Seconds(30))
+        .protocols(["my-subprotocol"])
+        .finish(SharedCfg::default())
+        .await
+        .unwrap()
+        .connect()
+        .await
+        .unwrap();
+
+    assert_eq!(conn.response().status(), StatusCode::SWITCHING_PROTOCOLS);
+    assert_eq!(
+        conn.response()
+            .headers()
+            .get(header::SEC_WEBSOCKET_PROTOCOL)
+            .map(|v| v.to_str().unwrap()),
+        Some("my-subprotocol")
+    );
+}
+
+#[ntex::test]
+async fn web_ws_subprotocol_none() {
+    use ntex::service::cfg::SharedCfg;
+    use ntex::time::Seconds;
+    use ntex::ws::WsClient;
+
+    let srv = test::server(async || {
+        App::new().service(web::resource("/").route(web::to(
+            |req: HttpRequest| async move {
+                // choose first supported protocol (none will match), convert to owned String
+                let protocol: Option<String> = ws::subprotocols(&req)
+                    .find(|p| *p == "unsupported")
+                    .map(String::from);
+
+                ws::start_using_subprotocol::<_, _, _, web::Error>(
+                    req,
+                    protocol,
+                    fn_factory_with_config(|_| async {
+                        Ok::<_, web::Error>(fn_service(service))
+                    }),
+                )
+                .await
+            },
+        )))
+    })
+    .await;
+
+    // client requests subprotocol that server doesn't support
+    let conn = WsClient::build(srv.url("/"))
+        .address(srv.addr())
+        .timeout(Seconds(30))
+        .protocols(["my-subprotocol"])
+        .finish(SharedCfg::default())
+        .await
+        .unwrap()
+        .connect()
+        .await
+        .unwrap();
+
+    assert_eq!(conn.response().status(), StatusCode::SWITCHING_PROTOCOLS);
+    // no protocol header in response
+    assert!(conn
+        .response()
+        .headers()
+        .get(header::SEC_WEBSOCKET_PROTOCOL)
+        .is_none());
+}
+
+#[ntex::test]
+async fn web_ws_protocols_parsing() {
+    use ntex::service::cfg::SharedCfg;
+    use ntex::time::Seconds;
+    use ntex::ws::WsClient;
+
+    let srv = test::server(async || {
+        App::new().service(web::resource("/").route(web::to(
+            |req: HttpRequest| async move {
+                // collect all requested protocols into owned Strings
+                let protocols: Vec<String> =
+                    ws::subprotocols(&req).map(String::from).collect();
+
+                // choose based on priority
+                let protocol = protocols
+                    .iter()
+                    .find(|p| *p == "proto2")
+                    .or_else(|| protocols.iter().find(|p| *p == "proto1"))
+                    .cloned();
+
+                ws::start_using_subprotocol::<_, _, _, web::Error>(
+                    req,
+                    protocol,
+                    fn_factory_with_config(|_| async {
+                        Ok::<_, web::Error>(fn_service(service))
+                    }),
+                )
+                .await
+            },
+        )))
+    })
+    .await;
+
+    // client requests multiple protocols (comma-separated)
+    let conn = WsClient::build(srv.url("/"))
+        .address(srv.addr())
+        .timeout(Seconds(30))
+        .protocols(["proto1", "proto2"])
+        .finish(SharedCfg::default())
+        .await
+        .unwrap()
+        .connect()
+        .await
+        .unwrap();
+
+    assert_eq!(conn.response().status(), StatusCode::SWITCHING_PROTOCOLS);
+    // server chooses proto2 (higher priority)
+    assert_eq!(
+        conn.response()
+            .headers()
+            .get(header::SEC_WEBSOCKET_PROTOCOL)
+            .map(|v| v.to_str().unwrap()),
+        Some("proto2")
+    );
 }
