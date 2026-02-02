8dcb4a1414c5317455b3ce465617599481ce7722
Nikolay Kim <fafhrd91@gmail.com>
2026-02-02 15:58:37 +0100

    Re-export h2 control (#740)

--- ntex/src/http/h2/mod.rs
+++ ntex/src/http/h2/mod.rs
@@ -3,7 +3,7 @@ mod default;
 pub(super) mod payload;
 mod service;
 
-pub use ntex_h2::{Control, ControlAck, ServiceConfig as Http2ServiceConfig};
+pub use ntex_h2::{Control, ControlAck, ServiceConfig as Http2ServiceConfig, control};
 
 pub use self::default::DefaultControlService;
 pub use self::payload::Payload;
