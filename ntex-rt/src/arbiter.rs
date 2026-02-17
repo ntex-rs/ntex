aada0c93e51343e8b2952caa16ed1e4ab33d8591
Nikolay Kim <fafhrd91@gmail.com>
2026-02-17 13:46:20 +0100

    Simplify rt::get_item() (#771)

--- ntex-rt/src/arbiter.rs
+++ ntex-rt/src/arbiter.rs
@@ -425,15 +425,11 @@ pub fn set_item<T: 'static>(item: T) {
 }
 
 /// Get a reference to a type previously inserted on this runtime's storage
-pub fn get_item<T: 'static, F, R>(f: F) -> R
-where
-    F: FnOnce(Option<&mut T>) -> R,
-{
+pub fn get_item<T: Clone + 'static>() -> Option<T> {
     STORAGE.with(move |cell| {
-        let mut st = cell.borrow_mut();
-        let item = st
-            .get_mut(&TypeId::of::<T>())
-            .and_then(|boxed| boxed.downcast_mut());
-        f(item)
+        cell.borrow()
+            .get(&TypeId::of::<T>())
+            .and_then(|boxed| boxed.downcast_ref())
+            .cloned()
     })
 }
