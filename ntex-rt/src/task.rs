4c2925cad67660c3d33a2fad79e6465e2bfeda8b
Nikolay Kim <fafhrd91@gmail.com>
2026-03-02 17:08:01 +0500

    Make task callbacks process wide

--- ntex-rt/src/task.rs
+++ ntex-rt/src/task.rs
@@ -1,27 +1,76 @@
-use std::{cell::Cell, ptr};
+use std::{sync::Arc, sync::atomic::AtomicUsize, sync::atomic::Ordering};
 
-thread_local! {
-    static CB: Cell<*const Callbacks> = const { Cell::new(ptr::null()) };
+// The Callbacks static holds a pointer to the global logger. It is protected by
+// the STATE static which determines whether LOGGER has been initialized yet.
+static mut CBS: Option<Arc<dyn CallbacksApi>> = None;
+
+static STATE: AtomicUsize = AtomicUsize::new(0);
+
+// There are three different states that we care about: the logger's
+// uninitialized, the logger's initializing (set_logger's been called but
+// LOGGER hasn't actually been set yet), or the logger's active.
+const UNINITIALIZED: usize = 0;
+const INITIALIZING: usize = 1;
+const INITIALIZED: usize = 2;
+
+trait CallbacksApi {
+    fn before(&self) -> Option<*const ()>;
+    fn enter(&self, _: *const ()) -> *const ();
+    fn exit(&self, _: *const ());
+    fn after(&self, _: *const ());
 }
 
-struct Callbacks {
-    before: Box<dyn Fn() -> Option<*const ()>>,
-    enter: Box<dyn Fn(*const ()) -> *const ()>,
-    exit: Box<dyn Fn(*const ())>,
-    after: Box<dyn Fn(*const ())>,
+#[allow(clippy::struct_field_names)]
+struct Callbacks<A, B, C, D> {
+    f_before: A,
+    f_enter: B,
+    f_exit: C,
+    f_after: D,
+}
+
+impl<A, B, C, D> CallbacksApi for Callbacks<A, B, C, D>
+where
+    A: Fn() -> Option<*const ()> + 'static,
+    B: Fn(*const ()) -> *const () + 'static,
+    C: Fn(*const ()) + 'static,
+    D: Fn(*const ()) + 'static,
+{
+    fn before(&self) -> Option<*const ()> {
+        (self.f_before)()
+    }
+    fn enter(&self, d: *const ()) -> *const () {
+        (self.f_enter)(d)
+    }
+    fn exit(&self, d: *const ()) {
+        (self.f_exit)(d);
+    }
+    fn after(&self, d: *const ()) {
+        (self.f_after)(d);
+    }
 }
 
 pub(crate) struct Data {
-    cb: &'static Callbacks,
+    cb: &'static dyn CallbacksApi,
     ptr: *const (),
 }
 
 impl Data {
+    #[allow(clippy::if_not_else)]
     pub(crate) fn load() -> Option<Data> {
-        let cb = CB.with(Cell::get);
+        // Acquire memory ordering guarantees that current thread would see any
+        // memory writes that happened before store of the value
+        // into `STATE` with memory ordering `Release` or stronger.
+        let cb = if STATE.load(Ordering::Acquire) != INITIALIZED {
+            None
+        } else {
+            #[allow(static_mut_refs)]
+            unsafe {
+                Some(CBS.as_ref().map(AsRef::as_ref).unwrap())
+            }
+        };
 
-        if let Some(cb) = unsafe { cb.as_ref() }
-            && let Some(ptr) = (*cb.before)()
+        if let Some(cb) = cb
+            && let Some(ptr) = cb.before()
         {
             return Some(Data { cb, ptr });
         }
@@ -32,16 +81,16 @@ impl Data {
     where
         F: FnOnce() -> R,
     {
-        let ptr = (*self.cb.enter)(self.ptr);
+        let ptr = self.cb.enter(self.ptr);
         let result = f();
-        (*self.cb.exit)(ptr);
+        self.cb.exit(ptr);
         result
     }
 }
 
 impl Drop for Data {
     fn drop(&mut self) {
-        (*self.cb.after)(self.ptr);
+        self.cb.after(self.ptr);
     }
 }
 
@@ -56,27 +105,23 @@ impl Drop for Data {
 ///
 /// Panics if task callbacks have already been set.
 pub unsafe fn task_callbacks<FBefore, FEnter, FExit, FAfter>(
-    before: FBefore,
-    enter: FEnter,
-    exit: FExit,
-    after: FAfter,
+    f_before: FBefore,
+    f_enter: FEnter,
+    f_exit: FExit,
+    f_after: FAfter,
 ) where
-    FBefore: Fn() -> Option<*const ()> + 'static,
-    FEnter: Fn(*const ()) -> *const () + 'static,
-    FExit: Fn(*const ()) + 'static,
-    FAfter: Fn(*const ()) + 'static,
+    FBefore: Fn() -> Option<*const ()> + 'static + Sync,
+    FEnter: Fn(*const ()) -> *const () + 'static + Sync,
+    FExit: Fn(*const ()) + 'static + Sync,
+    FAfter: Fn(*const ()) + 'static + Sync,
 {
-    CB.with(|cb| {
-        assert!(cb.get().is_null(), "Spawn callbacks already set");
-
-        let new: *mut Callbacks = Box::leak(Box::new(Callbacks {
-            before: Box::new(before),
-            enter: Box::new(enter),
-            exit: Box::new(exit),
-            after: Box::new(after),
-        }));
-        cb.replace(new);
+    let new = Arc::new(Callbacks {
+        f_before,
+        f_enter,
+        f_exit,
+        f_after,
     });
+    let _ = set_cbs(new);
 }
 
 /// # Safety
@@ -88,25 +133,46 @@ pub unsafe fn task_callbacks<FBefore, FEnter, FExit, FAfter>(
 ///
 /// Returns false if task callbacks have already been set.
 pub unsafe fn task_opt_callbacks<FBefore, FEnter, FExit, FAfter>(
-    before: FBefore,
-    enter: FEnter,
-    exit: FExit,
-    after: FAfter,
+    f_before: FBefore,
+    f_enter: FEnter,
+    f_exit: FExit,
+    f_after: FAfter,
 ) -> bool
 where
-    FBefore: Fn() -> Option<*const ()> + 'static,
-    FEnter: Fn(*const ()) -> *const () + 'static,
-    FExit: Fn(*const ()) + 'static,
-    FAfter: Fn(*const ()) + 'static,
+    FBefore: Fn() -> Option<*const ()> + Sync + 'static,
+    FEnter: Fn(*const ()) -> *const () + Sync + 'static,
+    FExit: Fn(*const ()) + Sync + 'static,
+    FAfter: Fn(*const ()) + Sync + 'static,
 {
-    CB.with(|cb| {
-        if cb.get().is_null() {
+    let new = Arc::new(Callbacks {
+        f_before,
+        f_enter,
+        f_exit,
+        f_after,
+    });
+    set_cbs(new).is_ok()
+}
+
+fn set_cbs(cbs: Arc<dyn CallbacksApi>) -> Result<(), ()> {
+    match STATE.compare_exchange(
+        UNINITIALIZED,
+        INITIALIZING,
+        Ordering::Acquire,
+        Ordering::Relaxed,
+    ) {
+        Ok(UNINITIALIZED) => {
             unsafe {
-                task_callbacks(before, enter, exit, after);
+                CBS = Some(cbs);
             }
-            true
-        } else {
-            false
+            STATE.store(INITIALIZED, Ordering::Release);
+            Ok(())
         }
-    })
+        Err(INITIALIZING) => {
+            while STATE.load(Ordering::Relaxed) == INITIALIZING {
+                std::hint::spin_loop();
+            }
+            Err(())
+        }
+        _ => Err(()),
+    }
 }
