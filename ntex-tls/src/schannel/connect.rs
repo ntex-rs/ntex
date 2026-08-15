73f9f401a23dca91aa1bf17a816267f4c871915d
Nikolay Kim <nikki@microsoft.com>
2026-08-15 14:56:32 +0200

    wip

--- ntex-tls/src/schannel/connect.rs
+++ ntex-tls/src/schannel/connect.rs
@@ -141,7 +141,7 @@ mod tests {
         let srv = factory.pipeline::<()>(&SharedCfg::default()).await.unwrap();
         assert!(srv.ready().await.is_ok());
         let result = srv
-            .call(Connect::new("").set_addr(Some(server.addr())), &())
+            .call(Connect::new("").set_addr(Some(server.addr())))
             .await;
         assert!(result.is_err());
         assert!(format!("{srv:?}").contains("TlsConnector"));
