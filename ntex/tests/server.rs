28e6306f890f16923c7d4a01a31bc8567583fc76
Nikolay Kim <fafhrd91@gmail.com>
2026-08-18 16:53:35 +0200

    wip

--- ntex/tests/server.rs
+++ ntex/tests/server.rs
@@ -1,11 +1,8 @@
-#[cfg(unix)]
-use std::io::{Read, Write};
 use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
-use std::{net, sync::Arc, sync::mpsc, thread, time};
+use std::{io::Read, io::Write, net, sync::Arc, sync::mpsc, thread, time};
 
 use ntex::server::{TestServer, build, build_with_state};
 use ntex::service::{State, cfg::SharedCfg, fn_service};
-#[cfg(unix)]
 use ntex::{codec::BytesCodec, io::Io, util::Bytes};
 
 #[test]
