61e9cbdbb7ada6a1348df4fa00b2102bd476c2c8
Nikolay Kim <fafhrd91@gmail.com>
2026-08-27 08:21:16 +0200

    Fix arbiter storage cleanup logic

--- ntex-server/CHANGES.md
+++ ntex-server/CHANGES.md
@@ -1,5 +1,9 @@
 # Changes
 
+## [4.0.0-beta.1] - 2026-08-26
+
+* Shutdown on SIGTERM is always graceful
+
 ## [4.0.0-beta.0] - 2026-08-24
 
 * Add app state management
--- ntex-server/src/pool.rs
+++ ntex-server/src/pool.rs
@@ -94,8 +94,9 @@ impl WorkerPool {
     #[must_use]
     /// Graceful shutdown.
     ///
-    /// Gracefully shuts down on SIGTERM, SIGSEGV, or SIGQUIT.
-    /// By default, graceful shutdown is disabled.
+    /// Gracefully shuts down on SIGSEGV or SIGQUIT and app panics.
+    /// Graceful shutdown is always enabled for SIGTERM.
+    /// By default, it is disabled for SIGSEGV and SIGQUIT and panics.
     pub fn graceful_shutdown(mut self) -> Self {
         self.graceful_shutdown = true;
         self
