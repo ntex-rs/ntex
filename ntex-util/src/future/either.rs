ba6e33d7583e8d92031e4ef5e1a06bdadc818ab4
Nikolay Kim <fafhrd91@gmail.com>
2026-09-21 16:43:51 +0200

    wip

--- ntex-util/src/future/either.rs
+++ ntex-util/src/future/either.rs
@@ -168,29 +168,34 @@ where
 
 #[cfg(test)]
 mod test {
+    use std::{cell::Cell, error::Error as _, future, rc::Rc};
+
+    use ntex_service::Pipeline;
+
     use super::*;
 
     #[test]
-    #[allow(clippy::unit_cmp)]
-    fn either() {
-        let mut e = Either::<(), ()>::Left(());
-        assert!(e.is_left());
-        assert!(!e.is_right());
-        assert!(e.left().is_some());
-        assert!(e.right().is_none());
-        e.as_ref();
-        e.as_mut();
+    fn accessors() {
+        let mut value = Either::<u8, String>::Left(10);
+        assert!(value.is_left());
+        assert!(!value.is_right());
+        assert_eq!(value.as_ref(), Either::Left(&10));
+        if let Either::Left(inner) = value.as_mut() {
+            *inner = 20;
+        }
+        assert_eq!(value.left(), Some(20));
 
-        let mut e = Either::<(), ()>::Right(());
-        assert!(!e.is_left());
-        assert!(e.is_right());
-        assert!(e.left().is_none());
-        assert!(e.right().is_some());
-        e.as_ref();
-        e.as_mut();
+        let mut value = Either::<u8, String>::Right("right".to_owned());
+        assert!(!value.is_left());
+        assert!(value.is_right());
+        assert_eq!(value.as_ref(), Either::Right(&"right".to_owned()));
+        if let Either::Right(inner) = value.as_mut() {
+            inner.push_str(" branch");
+        }
+        assert_eq!(value.right().as_deref(), Some("right branch"));
 
-        assert_eq!(Either::<(), ()>::Left(()).into_inner(), ());
-        assert_eq!(Either::<(), ()>::Right(()).into_inner(), ());
+        assert_eq!(Either::<u8, u8>::Left(1).into_inner(), 1);
+        assert_eq!(Either::<u8, u8>::Right(2).into_inner(), 2);
 
         assert_eq!(
             format!("{}", Either::<_, &'static str>::Left("test")),
@@ -200,5 +205,135 @@ mod test {
             format!("{}", Either::<&'static str, _>::Right("test")),
             "test"
         );
+    }
+
+    #[ntex::test]
+    async fn future() {
+        let left: Either<_, future::Ready<u8>> = Either::Left(future::ready(10));
+        assert_eq!(left.await, 10);
+
+        let right: Either<future::Ready<u8>, _> = Either::Right(future::ready(20));
+        assert_eq!(right.await, 20);
+    }
+
+    #[derive(Debug)]
+    struct TestError {
+        message: &'static str,
+        source: Option<io::Error>,
+    }
+
+    impl fmt::Display for TestError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            f.write_str(self.message)
+        }
+    }
+
+    impl error::Error for TestError {
+        fn source(&self) -> Option<&(dyn error::Error + 'static)> {
+            self.source
+                .as_ref()
+                .map(|source| source as &(dyn error::Error + 'static))
+        }
+    }
+
+    #[test]
+    fn errors() {
+        let left = Either::<TestError, TestError>::Left(TestError {
+            message: "left",
+            source: Some(io::Error::other("left source")),
+        });
+        assert_eq!(left.to_string(), "left");
+        assert_eq!(left.source().unwrap().to_string(), "left source");
+
+        let right = Either::<TestError, TestError>::Right(TestError {
+            message: "right",
+            source: Some(io::Error::other("right source")),
+        });
+        assert_eq!(right.to_string(), "right");
+        assert_eq!(right.source().unwrap().to_string(), "right source");
+
+        let left: io::Error = Either::<TestError, io::Error>::Left(TestError {
+            message: "converted",
+            source: None,
+        })
+        .into();
+        assert_eq!(left.kind(), io::ErrorKind::Other);
+        assert!(left.to_string().contains("converted"));
+
+        let right: io::Error = Either::<TestError, io::Error>::Right(io::Error::new(
+            io::ErrorKind::TimedOut,
+            "right error",
+        ))
+        .into();
+        assert_eq!(right.kind(), io::ErrorKind::TimedOut);
+        assert_eq!(right.to_string(), "right error");
+    }
+
+    #[derive(Default)]
+    struct ServiceState {
+        ready: Cell<bool>,
+        called: Cell<bool>,
+        shutdown: Cell<bool>,
+    }
+
+    struct TestService {
+        state: Rc<ServiceState>,
+        response: u8,
+    }
+
+    impl Service<(), u8> for TestService {
+        type Res = u8;
+        type Error = ();
+
+        async fn ready(&self, _: Ctx<'_, Self>) -> Result<(), Self::Error> {
+            self.state.ready.set(true);
+            Ok(())
+        }
+
+        async fn call(&self, req: u8, _: Ctx<'_, Self>) -> Result<Self::Res, Self::Error> {
+            self.state.called.set(true);
+            Ok(req + self.response)
+        }
+
+        async fn shutdown(&self, _: Ctx<'_, Self>) {
+            self.state.shutdown.set(true);
+        }
+    }
+
+    #[ntex::test]
+    async fn service() {
+        async fn check(service: Either<TestService, TestService>, response: u8) {
+            let state = match &service {
+                Either::Left(service) | Either::Right(service) => service.state.clone(),
+            };
+            let service = Pipeline::new((), service);
+
+            assert_eq!(service.ready().await, Ok(()));
+            assert!(state.ready.get());
+            assert_eq!(service.call(5).await, Ok(5 + response));
+            assert!(state.called.get());
+            service.shutdown().await;
+            assert!(state.shutdown.get());
+        }
+
+        let state = Rc::new(ServiceState::default());
+        check(
+            Either::Left(TestService {
+                state,
+                response: 10,
+            }),
+            10,
+        )
+        .await;
+
+        let state = Rc::new(ServiceState::default());
+        check(
+            Either::Right(TestService {
+                state,
+                response: 20,
+            }),
+            20,
+        )
+        .await;
     }
 }
